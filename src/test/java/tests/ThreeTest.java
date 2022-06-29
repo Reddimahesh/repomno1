70dddfcbe16b5320fee157e0cc0e9b2b95238ede
Reddimahesh <reddimahesh733@gmail.com>
2022-06-29 10:02:17 +0530

    first commit

---
+++ src/test/java/tests/ThreeTest.java
@@ -0,0 +1,21 @@
+package tests;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import resources.Base;
+
+public class ThreeTest extends Base {
+	 WebDriver driver;
+@Test
+public void testThree() throws InterruptedException, IOException {
+	System.out.println("TestThree");
+	
+    driver = initializeDriver();
+	driver.get("http://www.tutorialsninja.com/demo/");
+	Thread.sleep(2000);
+	driver.close();
+}
+}
